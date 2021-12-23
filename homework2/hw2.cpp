60cf9b627f0cd3571681d0eddba3eced85959f86
MULLIAH9 <95577477+MULLIAH9@users.noreply.github.com>
2021-12-23 13:08:06 +0300

    Update hw2.cpp

--- homework2/hw2.cpp
+++ homework2/hw2.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char** argv) {
     if (argc == 2) {
         std::ifstream infile(argv[1]);
-        std::vector < std::pair < double, double >> obstacles;
+        std::vector < std::pair < double, double >> obst;
         double h;
         double x = 0;
 
@@ -25,19 +25,19 @@ int main(int argc, char** argv) {
                 std::cout << "0" << std::endl;
                 return 0;
             }
-            if (target > size(obstacles) - 1) {
+            if (target > size(obst) - 1) {
                 double x_obs, h_obs;
                 if (infile >> x_obs >> h_obs) {
                     obstacles.push_back(std::make_pair(x_obs, h_obs));
                 } else {
-                    std::cout << size(obstacles) << std::endl;
+                    std::cout << size(obst) << std::endl;
                     return 0;
                 }
             }
 
-            double t = std::abs((x - obstacles[target].first) / vx);
+            double t = std::abs((x - obst[target].first) / vx);
 
-            x = obstacles[target].first;
+            x = obst[target].first;
             h = h + vy * t + 0.5 * g * t * t;
             vy = vy + g * t;
 
@@ -45,7 +45,7 @@ int main(int argc, char** argv) {
                 std::cout << interval << std::endl;
                 return 0;
             }
-            if (h > obstacles[target].second) {
+            if (h > obst[target].second) {
                 (vx > 0) ? interval++ : interval--;
             } else {
                 vx = vx * (-1);
