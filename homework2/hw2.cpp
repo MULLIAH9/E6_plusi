3bc34f8994fd70745c97765ada76d90dd7b88b22
MULLIAH9 <95577477+MULLIAH9@users.noreply.github.com>
2021-12-23 19:47:15 +0300

    Update hw2.cpp

--- homework2/hw2.cpp
+++ homework2/hw2.cpp
@@ -4,6 +4,57 @@
 #include <vector>
 #include <cmath>
 
-int main() {
-       
+int main(int argc, char** argv) {
+    if (argc == 2) {
+        std::ifstream infile(argv[1]);
+
+        std::vector < std::pair < double, double >> wall;
+        double h;
+        double x = 0;
+
+        infile >> h;
+
+        double vx, vy;
+        infile >> vx >> vy;
+
+        double g = -9.81;
+
+        int interval = 0;
+        int size = 0;
+
+        while (true) {
+            int target = (vx > 0) ? interval : interval - 1;
+            if (target < 0) {
+                std::cout << "0" << std::endl;
+                return 0;
+            }
+            if (target > size - 1) {
+                double x_w, h_w;
+                if (infile >> x_w >> h_w) {
+                    obstacles.push_back(std::make_pair(x_w, h_w));
+                    size++;
+                } else {
+                    std::cout << size << std::endl;
+                    return 0;
+                }
+
+            }
+
+            double t = std::abs((x - wall[target].first) / vx);
+
+            x = wall[target].first;
+            h = h + vy * t + 0.5 * g * t * t;
+            vy = vy + g * t;
+
+            if (h <= 0) {
+                std::cout << interval << std::endl;
+                return 0;
+            }
+            if (h > wall[target].second) {
+                (vx > 0) ? interval++ : interval--;
+            } else {
+                vx = vx * (-1);
+            }
+        }
+    }
 }
